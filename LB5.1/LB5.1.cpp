d110770e220d111d610f178ad618a8bd285d11c3
OlehMalskyi <omalskij3@gmail.com>
2023-10-16 19:47:27 +0300

    lb5.1 commit

--- LB5.1/LB5.1.cpp
+++ LB5.1/LB5.1.cpp
@@ -1,36 +1,25 @@
-// Lab_05_1.cpp
-// < Мальський Олег >
-// Лабораторна робота № 5.1
-// Функції, що містять арифметичний вираз
-// Варіант 15
-
 #include <iostream>
-#include <math.h>
 #include <cmath>
-
-
 
 using namespace std;
 
 double h(const double s, const double b);
 
-
 int main()
 {
-    double s, t;
+	double s, t;
 
-    cout << "s = "; cin >> s;
-    cout << "t = "; cin >> t;
+	cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
 
-    double c = h(s, t * t) + h(t, 1 + s * s) / 1 + h(s * t, 1);
+	double c = h(s, t * t) + h(t, 1 + s * s) / 1 + h(s * t, 1);
 
-    cout << "c = " << c << endl;
+	cout << "c = " << c << endl;
 
-    return 0;
+	return 0;
 }
-
 
 double h(const double x, const double y)
 {
-    return (x * x + sin(x) * y + y * y) / (1 + x * x + y * y);
+	return (x * x + sin(x * y) + y * y) / (1 + x * x + y * y);
 }
