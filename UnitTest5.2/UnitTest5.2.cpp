ef339e0866a5d5f60da28dc74319688658fffcfe
OlehMalskyi <omalskij3@gmail.com>
2023-10-17 19:57:31 +0300

    lb5.2 commit

---
+++ UnitTest5.2/UnitTest5.2.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../LB5.2/LB5.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest52
+{
+	TEST_CLASS(UnitTest52)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double c;
+			int n = 0;
+			c = S(1, 0.00001, n, 1);
+			Assert::AreEqual(c, 0.540302, 0.00001);
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			double a;
+			int n = 0;
+			a = A(1, 5, 1);
+			Assert::AreEqual(a, -0.0111111, 0.00001);
+		}
+	};
+}
